3e9bae45d14b5f4718c9155f0e8156b8f1be08e6
hyper-lang <58451170+hyper-lang@users.noreply.github.com>
2021-01-29 19:47:13 -0700

    Update react.js

--- react.js
+++ react.js
@@ -5,5 +5,8 @@ Class Test extends React.Component{
     return(<h1>test</h1>);
   }
 }
+					 
+const simple = <h1>This is a simple element</h1>
            
 ReactDOM.render(<Test />, document.getElementById('test'));
+ReactDOM.render(simple, document.getElementById('test'));
